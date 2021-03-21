dae30b2c48074e74f5481d3d06bf1628c93c56e4
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2021-03-21 14:14:40 +0900

    Save1

---
+++ spec/system/usersessions_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Usersessions", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+  describe 'ログイン前' do
+    context 'フォームの入力値が正常' do
+      it 'ログイン処理が成功する'
+    end
+    context 'フォームが未入力' do
+      it 'ログイン処理が失敗する'
+    end
+  end
+
+  describe 'ログイン後' do
+    context 'ログアウトボタンをクリック' do
+      it 'ログアウト処理が成功する'
+    end
+  end
+end
